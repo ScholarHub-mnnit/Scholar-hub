38c1c8e378e551bbde2302b070cc5254fd6f4f0c
harshi1298 <11082004harshit@gmail.com>
2024-10-29 11:19:53 +0530

    course tempelate completed

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ import morgan from 'morgan';
 import globalerrorhandler from './controller/errorcontroller.js';
 import authroute from './router/authrouter.js';
 import userroute from './router/userroute.js';
+import courseroute from './router/courserouter.js';
+import taskroute from './router/taskrouter.js';
 const app = express();
 
 app.use(express.json());
@@ -11,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use('/api/auth', authroute);
 app.use('/api/user', userroute);
+app.use('/api/course', courseroute);
+app.use('/api/task', taskroute);
 app.use(globalerrorhandler);
 
 export default app;
---
+++ server/controller/coursecontroller.js
@@ -0,0 +1,7 @@
+import asynchandler from '../utils/asynchandler';
+
+export const addcourse = asynchandler();
+export const updatecourse = asynchandler();
+export const getall = asynchandler();
+export const delcourse = asynchandler();
+export const deleteall = asynchandler();
