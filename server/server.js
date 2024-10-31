f38e3314a11f131193a26364cfa74d3eea6c6f33
dakshdagwar1234 <dakshdagwar@gmail.com>
2024-10-31 11:32:39 +0530

    a

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import app from './app.js';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: './.env' });
+dotenv.config({ path: './sample.env' });
 
 // const port = 8000;
 const port = process.env.PORT || 3000;
